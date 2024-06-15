5f12d7b9dd87c570fbe4b5ea26c97768f7dfcc4d
Aozzy-glitch <amabdi9867@gmail.com>
2024-06-15 08:00:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aozzy',
+  webDir: 'dist'
+};
+
+export default config;
